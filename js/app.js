0ba4075565786b0942c123ba9653b5125fd3cb32
Yisusad <jesuscheoz@gmail.com>
2021-02-17 15:05:47 -0300

    Selecionando elementos del DOM

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const botonNumero = document.querySelectorAll('.numero');
+const botonOperaciones = document.querySelectorAll('.operaciones');
+const botonIgual = document.getElementById('igual');
+const botonLimpiar = document.getElementById('limpiar');
+let resultado = document.getElementById('result');
